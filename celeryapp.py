a1f7ec98e1008a5622c2f965472649ee8d0c05ca
root <amirbehvandi747@gmail.com>
2020-03-17 15:49:33 +0000

    second change

--- celeryapp.py
+++ celeryapp.py
@@ -1,3 +1,13 @@
 from celery import Celery
-
+from kombu import Queue
 app = Celery('tasks',brocker='ampq://guset:guset@172.17.0.2:5672/celery_vhost',backend='rpc://')
+
+
+app.conf.beat_schedule = {
+    'add-every-30-seconds': {
+        'task': 'tasks.add',
+        'schedule': 30.0,
+        'args': (16, 16)
+    },
+}
+app.conf.timezone = 'UTC'
